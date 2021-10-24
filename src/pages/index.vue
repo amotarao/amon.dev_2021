20e64250939b973206d182774df75f3c5384f6ab
Amon Sawamura <amon.sawamura@gmail.com>
2021-10-25 01:04:26 +0900

    add layout

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,9 +2,14 @@
   <div>
     <section :class="$style.section">
       <h1 :class="$style.title">Amon Sawamura</h1>
+      <div :class="$style.image">ここになにか置きたい</div>
       <ul :class="$style.link_list">
-        <li><a href="https://twitter.com/amotarao">Twitter</a></li>
-        <li><a href="http://github.com/amotarao">GitHub</a></li>
+        <li :class="$style.link_item">
+          <a :class="$style.link_link" href="https://twitter.com/amotarao"><FaIcon :icon="['fab', 'twitter']" /></a>
+        </li>
+        <li :class="$style.link_item">
+          <a :class="$style.link_link" href="http://github.com/amotarao"><FaIcon :icon="['fab', 'github']" /></a>
+        </li>
       </ul>
     </section>
   </div>
@@ -19,29 +24,84 @@ export default Vue.extend({});
 <style lang="scss" module>
 .section {
   display: grid;
-  grid-template-areas: 'title link_list';
+  grid-template-areas:
+    'image image'
+    'title link_list';
+  grid-template-rows: 1fr auto;
+  grid-template-columns: 1fr auto;
+  gap: 1rem;
   width: 100vw;
   height: 100vh;
-  padding: 1em;
+  padding: 2rem 1rem;
 
   @include pc {
-    padding: 2em 4em;
+    gap: 2rem;
+    padding: 2rem 4rem;
   }
 }
 
 .title {
   grid-area: title;
   align-self: flex-end;
+  padding: 0.5rem 0;
   font-family: $urbanist;
-  font-size: 40px;
+  font-size: 1.5rem;
   font-weight: 400;
   letter-spacing: 0.08em;
+
+  @include pc {
+    padding: 0.75rem 0;
+    font-size: 2.5rem;
+  }
+}
+
+.image {
+  display: grid;
+  grid-area: image;
+  place-items: center;
+  align-self: stretch;
+  justify-self: stretch;
+  font-family: $zen-kaku-gothic-new;
+  font-size: 0.8rem;
+  color: #999;
+  letter-spacing: 0.12em;
+  border: dashed 2px #999;
 }
 
 .link_list {
+  display: flex;
   grid-area: link_list;
+  gap: 0.5rem;
   align-self: flex-end;
   justify-self: flex-end;
   list-style: none;
+
+  @include pc {
+    gap: 0.5rem;
+  }
+}
+
+.link_itrem {
+  //
+}
+
+.link_link {
+  display: block;
+  padding: 0.5rem;
+
+  @include pc {
+    padding: 0.75rem;
+  }
+
+  svg {
+    width: 1.8rem;
+    height: 1.8rem;
+    vertical-align: bottom;
+
+    @include pc {
+      width: 2.5rem;
+      height: 2.5rem;
+    }
+  }
 }
 </style>

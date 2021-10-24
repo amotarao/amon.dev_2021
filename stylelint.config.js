446c08851a69ca1f188c490ac691586d551d7591
Amon Sawamura <amon.sawamura@gmail.com>
2021-10-25 00:02:10 +0900

    fix stylelint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
-  // add your custom config here
-  // https://stylelint.io/user-guide/configuration
-  rules: {},
+  plugins: ['stylelint-prettier'],
+  extends: ['stylelint-config-recommended-scss'],
+  rules: {
+    'prettier/prettier': true,
+  },
 };

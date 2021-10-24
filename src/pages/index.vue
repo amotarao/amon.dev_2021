c6e70010eb37d56236e4897992e63ef3b8ab9aac
Amon Sawamura <amon.sawamura@gmail.com>
2021-10-25 00:22:55 +0900

    add section

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <Tutorial />
+  <div>
+    <section :class="$style.section">
+      <h1 :class="$style.title">Amon Sawamura</h1>
+      <ul :class="$style.link_list">
+        <li><a href="https://twitter.com/amotarao">Twitter</a></li>
+        <li><a href="http://github.com/amotarao">GitHub</a></li>
+      </ul>
+    </section>
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,3 +15,29 @@ import Vue from 'vue';
 
 export default Vue.extend({});
 </script>
+
+<style lang="scss" module>
+.section {
+  display: grid;
+  grid-template-areas: 'title link_list';
+  width: 100vw;
+  height: 100vh;
+  padding: 1em;
+
+  @include pc {
+    padding: 2em 4em;
+  }
+}
+
+.title {
+  grid-area: title;
+  align-self: flex-end;
+}
+
+.link_list {
+  grid-area: link_list;
+  align-self: flex-end;
+  justify-self: flex-end;
+  list-style: none;
+}
+</style>

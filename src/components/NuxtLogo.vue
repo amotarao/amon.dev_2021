bd720530f568fc351dd55813fb2087db2de0b0a7
Amon Sawamura <amon.sawamura@gmail.com>
2021-10-24 23:05:54 +0900

    fix lint

--- src/components/NuxtLogo.vue
+++ src/components/NuxtLogo.vue
@@ -7,6 +7,12 @@
   </svg>
 </template>
 
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({});
+</script>
+
 <style>
 .nuxt-logo {
   height: 180px;
--- src/components/Tutorial.vue
+++ src/components/Tutorial.vue
@@ -86,3 +86,9 @@
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({});
+</script>
